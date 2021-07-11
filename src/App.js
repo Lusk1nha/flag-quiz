65d2e43ad80586aefdb4fabdd91e67b6b4efac73
unknown <lucaspedro517@gmail.com>
2021-07-11 02:58:18 -0300

    setup react-repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Question } from './components/Question';
+
+import './styles/app.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id={`home`}>
+      <div className="questions-container">
+        <h2 className="title">Que país é esse?</h2>
+
+        <Question />
+      </div>
     </div>
   );
 }
---
+++ src/components/Question.jsx
@@ -0,0 +1,11 @@
+export function Question() {
+  return (
+    <form className="question">
+      <div className="flag-image">
+        Hello
+      </div>
+
+      text
+    </form>
+  );
+}
